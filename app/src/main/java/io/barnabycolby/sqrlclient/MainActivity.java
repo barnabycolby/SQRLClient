a6c2a2b987e58a43adba5a8054decc79901f5c5e
Barnaby Colby <barnaby.colby@gmail.com>
2015-11-10 22:52:45 +0000

    When application is started via a uri scheme intent then the uri is displayed.

--- app/src/main/java/io/barnabycolby/sqrlclient/MainActivity.java
+++ app/src/main/java/io/barnabycolby/sqrlclient/MainActivity.java
@@ -2,6 +2,9 @@ package io.barnabycolby.sqrlclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        TextView uriTextView = (TextView)findViewById(R.id.URITextView);
+        if (uriTextView == null || uri == null) {
+            return;
+        }
+        uriTextView.setText(uri.toString());
     }
 }
