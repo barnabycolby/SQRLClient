702a6dd1fc2a32d3cfffdbebfb82cfca2e75caf7
Barnaby Colby <barnaby.colby@gmail.com>
2015-11-21 13:31:54 +0000

    Moved tests to top of file and made small naming changes to tests.

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/MainActivityTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/MainActivityTest.java
@@ -15,6 +15,41 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         super(MainActivity.class);
     }
 
+    public void testSetURITextViewToLowercaseSQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("sqrl");
+    }
+
+    public void testSetURITextViewToLowercaseQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("qrl");
+    }
+
+    public void testSetURITextViewToUppercaseSQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("SQRL");
+    }
+    
+    public void testSetURITextViewToUppercaseQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("QRL");
+    }
+
+    public void testShowErrorMessageForUnsupportedScheme() {
+        // Create the URL with the unsupported scheme and start the activity
+        String scheme = "lsx";
+        String sqrlUri = scheme + "://sqrl-login.appspot.com/sqrl/auth?nut=7f8f1878ca7b4d3333daa8cfe3fe08f6";
+        MainActivity mainActivity = startActivityWithGivenURI(sqrlUri);
+
+        // Generate the expected text output
+        Resources resources = mainActivity.getResources();
+        String expected = resources.getString(R.string.unknown_scheme, scheme);
+
+        // Get the URI text box and verify the uri matches
+        TextView uriTextView = (TextView)mainActivity.findViewById(R.id.URITextView);
+        assertNotNull("uriTextView is null", uriTextView);
+        assertEquals(expected, uriTextView.getText());
+    }
+
+    public void testshowErrorMessageIfURIHasNoNut() {
+    }
+
     private void startActivityWithGivenSchemeAndTestTextViewMatchesURI(String scheme) {
         String sqrlUri = scheme + "://sqrl-login.appspot.com/sqrl/auth?nut=19d9d15d103aa22dfb59f4d6b39e98b2";
         MainActivity mainActivity = startActivityWithGivenURI(sqrlUri);
@@ -40,37 +75,5 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         assertNotNull("mainActivity is null", mainActivity);
 
         return mainActivity;
-    }
-
-    public void testsetURITextViewToLowercaseSQRLURI() {
-        startActivityWithGivenSchemeAndTestTextViewMatchesURI("sqrl");
-    }
-
-    public void testsetURITextViewToLowercaseQRLURI() {
-        startActivityWithGivenSchemeAndTestTextViewMatchesURI("qrl");
-    }
-
-    public void testsetURITextViewToUppercaseSQRLURI() {
-        startActivityWithGivenSchemeAndTestTextViewMatchesURI("SQRL");
-    }
-    
-    public void testsetURITextViewToUppercaseQRLURI() {
-        startActivityWithGivenSchemeAndTestTextViewMatchesURI("QRL");
-    }
-
-    public void testSqrlScheme_showErrorMessageForUnsupportedScheme() {
-        // Create the URL with the unsupported scheme and start the activity
-        String scheme = "lsx";
-        String sqrlUri = scheme + "://sqrl-login.appspot.com/sqrl/auth?nut=7f8f1878ca7b4d3333daa8cfe3fe08f6";
-        MainActivity mainActivity = startActivityWithGivenURI(sqrlUri);
-
-        // Generate the expected text output
-        Resources resources = mainActivity.getResources();
-        String expected = resources.getString(R.string.unknown_scheme, scheme);
-
-        // Get the URI text box and verify the uri matches
-        TextView uriTextView = (TextView)mainActivity.findViewById(R.id.URITextView);
-        assertNotNull("uriTextView is null", uriTextView);
-        assertEquals(expected, uriTextView.getText());
     }
 }
