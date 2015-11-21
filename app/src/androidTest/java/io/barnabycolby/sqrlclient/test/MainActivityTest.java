8cfc72f8d7794c748e69da578047544f54c2ef95
Barnaby Colby <barnaby.colby@gmail.com>
2015-11-21 13:31:50 +0000

    Check URI scheme is supported (either SQRL or QRL).

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/MainActivityTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/MainActivityTest.java
@@ -4,6 +4,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.content.Intent;
 import android.net.Uri;
 import android.widget.TextView;
+import android.content.res.Resources;
 
 import io.barnabycolby.sqrlclient.MainActivity;
 import io.barnabycolby.sqrlclient.R;
@@ -14,10 +15,22 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         super(MainActivity.class);
     }
 
-    public void testSqrlScheme_setURITextBasedOnIntent() {
-        // Create an Intent with a sqrl URI
-        String sqrlUri = "sqrl://sqrl-login.appspot.com/sqrl/auth?nut=19d9d15d103aa22dfb59f4d6b39e98b2";
-        Intent sqrlSchemeIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(sqrlUri));
+    private void startActivityWithGivenSchemeAndTestTextViewMatchesURI(String scheme) {
+        String sqrlUri = scheme + "://sqrl-login.appspot.com/sqrl/auth?nut=19d9d15d103aa22dfb59f4d6b39e98b2";
+        MainActivity mainActivity = startActivityWithGivenURI(sqrlUri);
+
+        // Get the URI text box and verify the uri matches
+        TextView uriTextView = (TextView)mainActivity.findViewById(R.id.URITextView);
+        assertNotNull("uriTextView is null", uriTextView);
+        assertEquals(sqrlUri, uriTextView.getText());
+
+        // Close the activity to prevent interference with future tests
+        mainActivity.finish();
+    }
+
+    private MainActivity startActivityWithGivenURI(String uri) {
+        // Create an Intent with the given URI
+        Intent sqrlSchemeIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
         sqrlSchemeIntent.addCategory(Intent.CATEGORY_DEFAULT);
         sqrlSchemeIntent.addCategory(Intent.CATEGORY_BROWSABLE);
 
@@ -26,9 +39,38 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         MainActivity mainActivity = getActivity();
         assertNotNull("mainActivity is null", mainActivity);
 
+        return mainActivity;
+    }
+
+    public void testsetURITextViewToLowercaseSQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("sqrl");
+    }
+
+    public void testsetURITextViewToLowercaseQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("qrl");
+    }
+
+    public void testsetURITextViewToUppercaseSQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("SQRL");
+    }
+    
+    public void testsetURITextViewToUppercaseQRLURI() {
+        startActivityWithGivenSchemeAndTestTextViewMatchesURI("QRL");
+    }
+
+    public void testSqrlScheme_showErrorMessageForUnsupportedScheme() {
+        // Create the URL with the unsupported scheme and start the activity
+        String scheme = "lsx";
+        String sqrlUri = scheme + "://sqrl-login.appspot.com/sqrl/auth?nut=7f8f1878ca7b4d3333daa8cfe3fe08f6";
+        MainActivity mainActivity = startActivityWithGivenURI(sqrlUri);
+
+        // Generate the expected text output
+        Resources resources = mainActivity.getResources();
+        String expected = resources.getString(R.string.unknown_scheme, scheme);
+
         // Get the URI text box and verify the uri matches
         TextView uriTextView = (TextView)mainActivity.findViewById(R.id.URITextView);
         assertNotNull("uriTextView is null", uriTextView);
-        assertEquals(sqrlUri, uriTextView.getText());
+        assertEquals(expected, uriTextView.getText());
     }
 }
--- app/src/main/java/io/barnabycolby/sqrlclient/MainActivity.java
+++ app/src/main/java/io/barnabycolby/sqrlclient/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.TextView;
 import android.net.Uri;
+import android.content.res.Resources;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get the uri from the data and the uri text box
         Intent intent = getIntent();
         Uri uri = intent.getData();
         TextView uriTextView = (TextView)findViewById(R.id.URITextView);
         if (uriTextView == null || uri == null) {
             return;
         }
+
+        // Check the scheme of the URL is recognised
+        Resources resources = getResources();
+        String uriScheme = uri.getScheme().toLowerCase();
+        if (!uriScheme.equals("sqrl") && !uriScheme.equals("qrl")) {
+            String errorMessage = resources.getString(R.string.unknown_scheme, uriScheme);
+            uriTextView.setText(errorMessage);
+            return;
+        }
+
         uriTextView.setText(uri.toString());
     }
 }
