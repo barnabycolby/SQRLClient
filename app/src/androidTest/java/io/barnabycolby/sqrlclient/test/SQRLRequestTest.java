bbc0b4398fd4a7caeba456f66c1cc00e7e99fac6
Barnaby Colby <barnaby.colby@gmail.com>
2015-11-26 00:25:53 +0000

    Extracted a setup method from existing test to avoid repeating code. Also split existing test into two tests so that each only has a single responsibility.

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/SQRLRequestTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/SQRLRequestTest.java
@@ -1,9 +1,8 @@
 package io.barnabycolby.sqrlclient.test;
 
 import android.support.test.runner.AndroidJUnit4;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.junit.Assert;
 import android.net.Uri;
 import java.net.URLConnection;
 
@@ -11,21 +10,30 @@ import io.barnabycolby.sqrlclient.sqrl.*;
 
 @RunWith(AndroidJUnit4.class)
 public class SQRLRequestTest {
-    @Test
-    public void createConnectionToTheCorrectURLWithCorrectRequestProperties() throws Exception {
-        // Create the SQRL URI
-        String baseUri = "://www.grc.com/sqrl?nut=P2Kr_4GB49GrwAF_kpDuJA&sfn=R1JD";
-        Uri uri = Uri.parse("sqrl" + baseUri);
-        SQRLUri sqrlUri = new SQRLUri(uri);
-        SQRLRequest request = new SQRLRequest(sqrlUri);
+    private Uri uri;
+    private SQRLUri sqrlUri;
+    private SQRLRequest request;
 
-        // Check the url used for the connection
+    @Before
+    public void setUp() throws Exception {
+        // Create the SQRL URI
+        uri = Uri.parse("sqrl://www.grc.com/sqrl?nut=P2Kr_4GB49GrwAF_kpDuJA&sfn=R1JD");
+        sqrlUri = new SQRLUri(uri);
+        request = new SQRLRequest(sqrlUri);
+    }
+
+    @Test
+    public void createConnectionToTheCorrectURL() throws Exception {
+        // Check the correct url was used
         URLConnection connection = request.getConnection();
         String actual = connection.getURL().toExternalForm();
-        String expected = "https" + baseUri;
+        String expected = "https://www.grc.com/sqrl?nut=P2Kr_4GB49GrwAF_kpDuJA&sfn=R1JD";
         Assert.assertEquals(expected, actual);
+    }
 
-        // Check the request properties are correct
+    @Test
+    public void setCorrectHeadersBasedOnURI() throws Exception {
+        URLConnection connection = request.getConnection();
         Assert.assertEquals(uri.getHost(), connection.getRequestProperty("Host"));
         Assert.assertEquals("SQRL/1", connection.getRequestProperty("User-Agent"));
         Assert.assertEquals("application/x-www-form-urlencoded", connection.getRequestProperty("Content-type"));
