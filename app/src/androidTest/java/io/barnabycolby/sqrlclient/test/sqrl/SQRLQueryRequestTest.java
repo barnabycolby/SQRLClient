671f89f4775c02198aeb22a0e751e799cb7df54a
Barnaby Colby <barnaby.colby@gmail.com>
2015-12-19 08:14:55 +0000

    Changed SQRLQueryRequest tests to follow the same pattern as the SQRLIdentRequest tests, essentially, testing the 2 methods that they implement, instead of testing the logic in SQRLRequest, as this should be tested in the SQRLRequestTest class.

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/sqrl/SQRLQueryRequestTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/sqrl/SQRLQueryRequestTest.java
@@ -1,53 +1,63 @@
 package io.barnabycolby.sqrlclient.test.sqrl;
 
-import android.net.Uri;
 import android.support.test.runner.AndroidJUnit4;
 
-import java.io.*;
-import java.net.HttpURLConnection;
+import io.barnabycolby.sqrlclient.sqrl.SQRLConnection;
+import io.barnabycolby.sqrlclient.sqrl.SQRLIdentity;
+import io.barnabycolby.sqrlclient.sqrl.SQRLQueryRequest;
+import io.barnabycolby.sqrlclient.sqrl.SQRLResponse;
+import io.barnabycolby.sqrlclient.sqrl.SQRLResponseFactory;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.*;
 
-import io.barnabycolby.sqrlclient.exceptions.TransientErrorException;
-import io.barnabycolby.sqrlclient.sqrl.*;
-
 @RunWith(AndroidJUnit4.class)
 public class SQRLQueryRequestTest {
-    private Uri uri;
-    private SQRLUri sqrlUri;
 
-    @Before
-    public void setUp() throws Exception {
-        // Create the SQRL URI
-        uri = Uri.parse("sqrl://www.grc.com/sqrl?nut=P2Kr_4GB49GrwAF_kpDuJA&sfn=R1JD");
-        sqrlUri = new SQRLUri(uri);
+    /**
+     * Create a subclass of SQRLQueryRequest so that we can verify it's protected methods.
+     */
+    private class SQRLQueryRequestPublic extends SQRLQueryRequest {
+        public SQRLQueryRequestPublic(SQRLConnection connection, SQRLIdentity sqrlIdentity, SQRLResponseFactory sqrlResponseFactory) {
+            super(connection, sqrlIdentity, sqrlResponseFactory);
+        }
+
+        @Override
+        public boolean areServerUnlockAndVerifyUnlockKeysRequired() {
+            return super.areServerUnlockAndVerifyUnlockKeysRequired();
+        }
+
+        @Override
+        public String getCommandString() {
+            return super.getCommandString();
+        }
     }
 
     @Test
-    public void correctlyGenerateQueryRequest() throws Exception {
-        // First, we need to mock the connection object
-        SQRLConnection connection = SQRLRequestTest.getMockSQRLConnection(this.sqrlUri);
-
-        // Mock the SQRLIdentity
-        String expectedClientValue = "dmVyPTENCmNtZD1xdWVyeQ0KaWRrPUpqbDJPaFV5UDkzTTE0LUFRM3N0WU1hb1oydnExQkhmbUFoeFdqTTFDdVUNCg";
-        String expectedServerValue = "c3FybDovL3d3dy5ncmMuY29tL3Nxcmw_bnV0PVAyS3JfNEdCNDlHcndBRl9rcER1SkEmc2ZuPVIxSkQ";
-        String signatureOfExpectedData = "je8rKDoBUnS0PdAyYNQQ-RpZ1YtI_bj4dTZCRnKDvTAcG1Vj_FQtPZlnKeajGFlZCJMH2JRWyBkRs5Y747drDw";
-        SQRLIdentity sqrlIdentity = SQRLRequestTest.getMockSQRLIdentity(expectedClientValue, expectedServerValue, signatureOfExpectedData);
+    public void commandStringShouldBeQry() throws Exception {
+        // Create the required mock objects
+        SQRLConnection connection = mock(SQRLConnection.class);
+        SQRLIdentity sqrlIdentity = mock(SQRLIdentity.class);
 
         // Next, instantiate a SQRLRequest object with the mocked objects
-        SQRLQueryRequest request = new SQRLQueryRequest(connection, sqrlIdentity, new MockSQRLResponseFactory());
+        SQRLQueryRequestPublic request = new SQRLQueryRequestPublic(connection, sqrlIdentity, new MockSQRLResponseFactory());
 
-        // Calculate what the expected data should be
-        String expectedData = "client=" + expectedClientValue;
-        expectedData += "&server=" + expectedServerValue;
-        expectedData += "&ids=" + signatureOfExpectedData;
+        // Assert the command string
+        Assert.assertEquals("query", request.getCommandString());
+    }
 
-        // Ask the request object to send the data, and then verify it
-        request.send();
-        String dataSent = connection.getOutputStream().toString();
-        Assert.assertEquals(expectedData, dataSent);
+    @Test
+    public void areServerUnlockAndVerifyUnlockKeysRequiredShouldReturnFalse() throws Exception {
+        // Create the required mock objects
+        SQRLConnection connection = mock(SQRLConnection.class);
+        SQRLIdentity sqrlIdentity = mock(SQRLIdentity.class);
+
+        // Next, instantiate a SQRLRequest object with the mocked objects
+        SQRLQueryRequestPublic request = new SQRLQueryRequestPublic(connection, sqrlIdentity, new MockSQRLResponseFactory());
+
+        // Assert the result of areServerUnlockAndVerifyUnlockKeysRequired
+        Assert.assertFalse(request.areServerUnlockAndVerifyUnlockKeysRequired());
     }
 }
