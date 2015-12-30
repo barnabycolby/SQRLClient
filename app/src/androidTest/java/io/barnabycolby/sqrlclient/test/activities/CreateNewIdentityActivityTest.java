0b1f72df16b3d0d5c9a3444d8d7f1ba7614e77e4
Barnaby Colby <barnaby.colby@gmail.com>
2015-12-30 17:37:40 +0000

    Added explanation text to the create new identity activity to explain to the user what they need to do.

---
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/CreateNewIdentityActivityTest.java
@@ -0,0 +1,29 @@
+package io.barnabycolby.sqrlclient.test.activities;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.TextView;
+
+import io.barnabycolby.sqrlclient.activities.CreateNewIdentityActivity;
+import io.barnabycolby.sqrlclient.R;
+
+public class CreateNewIdentityActivityTest extends ActivityInstrumentationTestCase2<CreateNewIdentityActivity> {
+    public CreateNewIdentityActivityTest() {
+        super(CreateNewIdentityActivity.class);
+    }
+
+    public void testExplanationTextIsDisplayed() {
+        // Get the activity
+        CreateNewIdentityActivity activity = getActivity();
+
+        // Get the expected explanation text
+        Resources resources = activity.getResources();
+        String expected = resources.getString(R.string.create_new_identity_explanation);
+
+        // Assert that the appropriate TextView is visible and shows the correct text
+        TextView explanationTextView = (TextView)activity.findViewById(R.id.CreateNewIdentityExplanationTextView);
+        assertEquals(expected, explanationTextView.getText());
+        assertEquals(View.VISIBLE, explanationTextView.getVisibility());
+    }
+}
