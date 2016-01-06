a7508bab9e40a83017bb0631b993b3a293a3ca91
Barnaby Colby <barnaby.colby@gmail.com>
2016-01-06 16:47:54 +0000

    Updated CreateNewIdentityActivity tests to handle the camera permissions dialog. Because the dialog is run outside of the application this required the use of the UiAutomator API, which in turn required the test to be built on an android instrumemtnation test instead of a functional test.

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/CreateNewIdentityActivityTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/CreateNewIdentityActivityTest.java
@@ -1,7 +1,13 @@
 package io.barnabycolby.sqrlclient.test.activities;
 
+import android.app.Activity;
 import android.content.res.Resources;
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -10,21 +16,32 @@ import android.widget.TextView;
 import io.barnabycolby.sqrlclient.activities.CreateNewIdentityActivity;
 import io.barnabycolby.sqrlclient.R;
 
-public class CreateNewIdentityActivityTest extends ActivityInstrumentationTestCase2<CreateNewIdentityActivity> {
-    private CreateNewIdentityActivity mActivity;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.Test;
 
-    public CreateNewIdentityActivityTest() {
-        super(CreateNewIdentityActivity.class);
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class CreateNewIdentityActivityTest {
+    private Activity mActivity;
+    private UiDevice mDevice;
+
+    @Rule
+    public ActivityTestRule mActivityTestRule = new ActivityTestRule(CreateNewIdentityActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        this.mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        this.mActivity = mActivityTestRule.getActivity();
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Test
+    public void testExplanationTextIsDisplayed() throws Exception {
+        allowCameraPermissions();
 
-        this.mActivity = getActivity();
-    }
-
-    public void testExplanationTextIsDisplayed() {
         // Get the expected explanation text
         Resources resources = mActivity.getResources();
         String expected = resources.getString(R.string.create_new_identity_explanation);
@@ -35,17 +52,32 @@ public class CreateNewIdentityActivityTest extends ActivityInstrumentationTestCa
         assertEquals(View.VISIBLE, explanationTextView.getVisibility());
     }
 
-    public void testProgressBarIsDisplayed() {
+    @Test
+    public void testProgressBarIsDisplayed() throws Exception {
+        allowCameraPermissions();
+
         // Find the progress bar and make sure it's visible
         ProgressBar progressBar = (ProgressBar)mActivity.findViewById(R.id.EntropyHarvesterProgressBar);
         assertNotNull(progressBar);
         assertEquals(View.VISIBLE, progressBar.getVisibility());
     }
 
-    public void testIdentityNameEditTextIsDisplayed() {
+    @Test
+    public void testIdentityNameEditTextIsDisplayed() throws Exception {
+        allowCameraPermissions();
+
         // Find the textbox and make sure it's visible
         EditText identityNameEditText = (EditText)mActivity.findViewById(R.id.IdentityNameEditText); 
         assertNotNull(identityNameEditText);
         assertEquals(View.VISIBLE, identityNameEditText.getVisibility());
+    }
+
+    private void allowCameraPermissions() throws Exception {
+        UiObject allowButton = mDevice.findObject(new UiSelector()
+                .text("Allow")
+                .className("android.widget.Button"));
+        if (allowButton.exists()) {
+            allowButton.click();
+        }
     }
 }
