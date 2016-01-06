5a6c56d06736e4f16be60b19f28b650824e2c4b2
Barnaby Colby <barnaby.colby@gmail.com>
2016-01-06 16:47:42 +0000

    Added code to check whether the system has any cameras available, displaying a detailed error message if not.

--- app/src/main/java/io/barnabycolby/sqrlclient/activities/CreateNewIdentityActivity.java
+++ app/src/main/java/io/barnabycolby/sqrlclient/activities/CreateNewIdentityActivity.java
@@ -1,7 +1,12 @@
 package io.barnabycolby.sqrlclient.activities;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import io.barnabycolby.sqrlclient.R;
 
@@ -11,10 +16,81 @@ import io.barnabycolby.sqrlclient.R;
  * The activity harvests entropy from the camera and uses it to create a new SQRL identity.
  */
 public class CreateNewIdentityActivity extends AppCompatActivity {
+    private String mUnrecoverableErrorKey = "error";
+    private String mErrorStringKey = "errorString";
+    private boolean mUnrecoverableErrorOccurred = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // Standard Android stuff
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_identity);
+
+        // Restore the value of mUnrecoverableErrorOccurred if it exists
+        if (savedInstanceState != null) {
+            this.mUnrecoverableErrorOccurred = savedInstanceState.getBoolean(this.mUnrecoverableErrorKey, false);
+        }
+
+        if (this.mUnrecoverableErrorOccurred) {
+            restore(savedInstanceState);
+        } else {
+            initialise();
+        }
+    }
+
+    private void initialise() {
+        // Get the camera manager
+        CameraManager cameraManager = (CameraManager)this.getSystemService(Context.CAMERA_SERVICE);
+        if (cameraManager == null) {
+            displayErrorMessage(R.string.camera_service_not_supported);
+            return;
+        }
+
+        // Check if the system has any cameras
+        try {
+            String[] cameraIds = cameraManager.getCameraIdList();
+            if (cameraIds.length == 0) {
+                displayErrorMessage(R.string.no_cameras);
+                return;
+            }
+        } catch (CameraAccessException ex) {
+            displayErrorMessage(ex.getMessage());
+            return;
+        }
+    }
+
+    private void restore(Bundle savedInstanceState) {
+        String errorMessage = savedInstanceState.getString(this.mErrorStringKey);
+        displayErrorMessage(errorMessage);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean(this.mUnrecoverableErrorKey, this.mUnrecoverableErrorOccurred);
+        if (this.mUnrecoverableErrorOccurred) {
+            TextView errorTextView = (TextView)this.findViewById(R.id.ErrorTextView);
+            outState.putString(this.mErrorStringKey, errorTextView.getText().toString());
+        }
+    }
+
+    private void displayErrorMessage(int stringId) {
+        String errorMessage = this.getResources().getString(stringId);
+        displayErrorMessage(errorMessage);
+    }
+
+    private void displayErrorMessage(String errorMessage) {
+        // Hide the main content
+        View mainContent = this.findViewById(R.id.MainContent);
+        mainContent.setVisibility(View.GONE);
+
+        // Display the error message
+        TextView errorTextView = (TextView)this.findViewById(R.id.ErrorTextView);
+        errorTextView.setText(errorMessage);
+        errorTextView.setVisibility(View.VISIBLE);
+
+        // Set the error flag so that instance state is handled correctly
+        this.mUnrecoverableErrorOccurred = true;
     }
 }
