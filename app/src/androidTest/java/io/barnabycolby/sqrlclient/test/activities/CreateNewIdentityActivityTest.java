489eefbd5ea87e013dae288a7d258bdf9a6ac566
Barnaby Colby <barnaby.colby@gmail.com>
2016-01-06 16:47:32 +0000

    Encapsulated getActivity code into test setUp method.

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/CreateNewIdentityActivityTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/CreateNewIdentityActivityTest.java
@@ -10,30 +10,33 @@ import io.barnabycolby.sqrlclient.activities.CreateNewIdentityActivity;
 import io.barnabycolby.sqrlclient.R;
 
 public class CreateNewIdentityActivityTest extends ActivityInstrumentationTestCase2<CreateNewIdentityActivity> {
+    private CreateNewIdentityActivity mActivity;
+
     public CreateNewIdentityActivityTest() {
         super(CreateNewIdentityActivity.class);
     }
 
-    public void testExplanationTextIsDisplayed() {
-        // Get the activity
-        CreateNewIdentityActivity activity = getActivity();
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
 
+        this.mActivity = getActivity();
+    }
+
+    public void testExplanationTextIsDisplayed() {
         // Get the expected explanation text
-        Resources resources = activity.getResources();
+        Resources resources = mActivity.getResources();
         String expected = resources.getString(R.string.create_new_identity_explanation);
 
         // Assert that the appropriate TextView is visible and shows the correct text
-        TextView explanationTextView = (TextView)activity.findViewById(R.id.CreateNewIdentityExplanationTextView);
+        TextView explanationTextView = (TextView)mActivity.findViewById(R.id.CreateNewIdentityExplanationTextView);
         assertEquals(expected, explanationTextView.getText());
         assertEquals(View.VISIBLE, explanationTextView.getVisibility());
     }
 
     public void testProgressBarIsDisplayed() {
-        // Get the activity
-        CreateNewIdentityActivity activity = getActivity();
-
         // Find the progress bar and make sure it's visible
-        ProgressBar progressBar = (ProgressBar)activity.findViewById(R.id.EntropyHarvesterProgressBar);
+        ProgressBar progressBar = (ProgressBar)mActivity.findViewById(R.id.EntropyHarvesterProgressBar);
         assertNotNull(progressBar);
         assertEquals(View.VISIBLE, progressBar.getVisibility());
     }
