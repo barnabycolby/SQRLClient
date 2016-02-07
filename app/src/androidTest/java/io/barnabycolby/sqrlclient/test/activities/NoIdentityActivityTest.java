146485fa94d4667de567504e3ecd8387d87e056c
Barnaby Colby <barnaby.colby@gmail.com>
2016-02-07 21:07:08 +0000

    Added a create new identity button to the NoIdentityActivity.

--- app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/NoIdentityActivityTest.java
+++ app/src/androidTest/java/io/barnabycolby/sqrlclient/test/activities/NoIdentityActivityTest.java
@@ -1,8 +1,13 @@
 package io.barnabycolby.sqrlclient.test.activities;
 
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import io.barnabycolby.sqrlclient.activities.CreateNewIdentityActivity;
 import io.barnabycolby.sqrlclient.activities.NoIdentityActivity;
 import io.barnabycolby.sqrlclient.R;
 
@@ -11,12 +16,14 @@ import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
@@ -32,8 +39,24 @@ public class NoIdentityActivityTest {
     }
 
     @Test
-    public void createIdentityTextIsDisplayed() {
+    public void noIdentityExplanationTextIsDisplayed() {
         onView(withId(R.id.NoIdentityExplanation)).check(matches(isDisplayed()));
         onView(withId(R.id.NoIdentityExplanation)).check(matches(withText(R.string.no_identity_explanation)));
+    }
+
+    @Test
+    public void createNewIdentityButtonTakesYouToTheCreateNewIdentityActivity() {
+        // Set up the activity monitor
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        ActivityMonitor activityMonitor = instrumentation.addMonitor(CreateNewIdentityActivity.class.getName(), null, false);
+
+        // Click the button
+        ViewInteraction createNewIdentityButton = onView(withId(R.id.CreateNewIdentityButton));
+        createNewIdentityButton.check(matches(isDisplayed()));
+        createNewIdentityButton.perform(click());
+
+        // Check the activity was started
+        CreateNewIdentityActivity createNewIdentityActivity = (CreateNewIdentityActivity)instrumentation.waitForMonitorWithTimeout(activityMonitor, 5000);
+        assertNotNull(createNewIdentityActivity);
     }
 }
--- app/src/main/java/io/barnabycolby/sqrlclient/activities/NoIdentityActivity.java
+++ app/src/main/java/io/barnabycolby/sqrlclient/activities/NoIdentityActivity.java
@@ -1,8 +1,11 @@
 package io.barnabycolby.sqrlclient.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import io.barnabycolby.sqrlclient.activities.CreateNewIdentityActivity;
 import io.barnabycolby.sqrlclient.R;
 
 /**
@@ -14,5 +17,13 @@ public class NoIdentityActivity extends AppCompatActivity {
         // Standard Android stuff
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_identity);
+    }
+
+    /**
+     * Called when the "Create New Identity" button is pressed.
+     */
+    public void createNewIdentity(View view) {
+        Intent intent = new Intent(this, CreateNewIdentityActivity.class);
+        startActivity(intent);
     }
 }
