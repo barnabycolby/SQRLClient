74fdbcb79593282eefc09a471a654ab40773e8d4
Barnaby Colby <barnaby.colby@gmail.com>
2016-02-22 23:41:12 +0000

    Added javadoc to new Helper class.

--- app/src/main/java/io/barnabycolby/sqrlclient/helpers/Helper.java
+++ app/src/main/java/io/barnabycolby/sqrlclient/helpers/Helper.java
@@ -3,7 +3,16 @@ package io.barnabycolby.sqrlclient.helpers;
 import android.app.Activity;
 import android.os.Looper;
 
+/**
+ * A helper class that contains static functions, encapsulating common functionality across the codebase.
+ */
 public class Helper {
+    /**
+     * Ensures that a given lambda is run on the UI thread.
+     *
+     * @param lambda  The lambda to run.
+     * @throws Exception  If the lambda throws an exception.
+     */
     public static void runOnUIThread(Activity activity, final Lambda lambda) throws Exception {
         if (Looper.getMainLooper().getThread() == Thread.currentThread()) {
             lambda.run();
